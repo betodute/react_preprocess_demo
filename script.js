73e9da74a7698c6bec3faf1be7076b279b3f4f4b
Alberto Duarte <betodute@gmail.com>
2022-07-01 20:15:02 -0700

    Basic React Component
    Playing around with a basic react temperature component with two props.

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ var element = React.createElement(
 );
 
 ReactDOM.render(element, document.getElementById('root'));
+
+var Temperature = function Temperature(props) {
+  return React.createElement(
+    "h1",
+    null,
+    "The current temperature is ",
+    props.degree,
+    " ",
+    props.unit
+  );
+};
+
+var element2 = React.createElement(Temperature, { degree: 25, unit: 'celsius' });
+
+ReactDOM.render(element2, document.getElementById('root2'));
--- src/script.js
+++ src/script.js
@@ -6,3 +6,15 @@ ReactDOM.render(
   element,
   document.getElementById('root')
 );
+
+
+const Temperature = (props) => {
+  return <h1>The current temperature is {props.degree} {props.unit}</h1>;
+};
+
+const element2 = <Temperature degree={25} unit={'celsius'}/>;
+
+ReactDOM.render(
+  element2,
+  document.getElementById('root2')
+);
